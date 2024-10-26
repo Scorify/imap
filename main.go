18559598002025143fee52f459103f7cf471dfda
1nv8rZim <msf9542@rit.edu>
2024-10-25 23:25:29 -0400

    Add schema

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 )
 
 type Schema struct {
-	Target string `key:"target"`
-	Port   int    `key:"port"`
+	Server   string `key:"server"`
+	Port     int    `key:"port" default:"143"`
+	Username string `key:"username"`
+	Password string `key:"password"`
+	Secure   bool   `key:"secure"`
 }
 
 func Validate(config string) error {
