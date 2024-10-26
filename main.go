bda2c12b9fb0b90b4cbb161f0a8c3bef86899546
1nv8rZim <msf9542@rit.edu>
2024-10-25 23:47:23 -0400

    implement `Run` function

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package imap
 
 import (
 	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
 
+	"github.com/emersion/go-imap/client"
 	"github.com/scorify/schema"
 )
 
@@ -32,6 +36,45 @@ func Run(ctx context.Context, config string) error {
 	err := schema.Unmarshal([]byte(config), &conf)
 	if err != nil {
 		return err
+	}
+
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return fmt.Errorf("context deadline is not set")
+	}
+
+	dialer := &net.Dialer{
+		Deadline: deadline,
+	}
+
+	var imapClient *client.Client
+	connStr := fmt.Sprintf("%s:%d", conf.Server, conf.Port)
+
+	if conf.Secure {
+		imapClient, err = client.DialWithDialerTLS(
+			dialer,
+			connStr,
+			&tls.Config{InsecureSkipVerify: true},
+		)
+	} else {
+		imapClient, err = client.DialWithDialer(
+			dialer,
+			connStr,
+		)
+	}
+	if err != nil {
+		return err
+	}
+	defer imapClient.Logout()
+
+	err = imapClient.Login(conf.Username, conf.Password)
+	if err != nil {
+		return fmt.Errorf("failed to login: %w", err)
+	}
+
+	_, err = imapClient.Select(conf.Mailbox, true)
+	if err != nil {
+		return fmt.Errorf("failed opening mailbox %q: %w", conf.Mailbox, err)
 	}
 
 	return nil
